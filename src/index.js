96d0479af69075287a5454dd37cb4df85fd8e305
lpatiny <gb252243>
2016-08-06 21:41:58 +0200

    Work in progress

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ var combinate = require('./combinate.js')
  * @returns {*} return the top 3 best association
  */
 
-var DEBUG=true;
-
+var DEBUG=false;
+var numberOperation=0;
 
 function associator(links, options) {
+
+    
+
     var options=options || {};
     var minTarget=options.minTarget || 0;
     var maxTarget=options.maxTarget || 2;
+    
     
     
     var sources=[];
@@ -27,19 +31,20 @@ function associator(links, options) {
         sources.push(
             {
                 sourceID: key,
+                possibleLinks: links[key],
                 possibleTargets: combinate(links[key], minTarget, maxTarget),
                 currentTargetPosition: 0,
-                currentTotalTargetsAssigned: new Array(sources.length),
-                currentTotalScore:  new Array(sources.length)
+                currentTotalTargetsAssigned: 0,
+                currentTotalScore: 1
             }
         );
     }
     
-    console.log(JSON.stringify(sources,"   "));
+    // console.log(JSON.stringify(sources,"   "));
     
     var targets = {};
     sources.map(function(source) {
-        source.possibleTargets.map(function(targetID) {
+        source.possibleLinks.map(function(targetID) {
             if (! targets[targetID]) {
                 targets[targetID]={
                     isUsed: false
@@ -47,48 +52,114 @@ function associator(links, options) {
             }
         });
     });
+    var numberOfTargets=Object.keys(targets).length;
     
-
+    var currentSource=-1;
     
-    var currentSource=0;
-    var currentTotalAssignedTargets=0;
-    
-    var limit=50;
+    var limit=5000000;
     var counter=0;
-    while (limit-- > 0 && currentSource>=0) {
-        // can we for the current source that we are evaluating still add a child ?
-        while(currentSource<(sources.length-1)) {
+    do {
+        if (currentSource < (sources.length-1)) {
             currentSource++;
-            sources[currentSource].currentTargetPosition=0;
+            sources[currentSource].currentTargetPosition=-1;
         }
-        // we should now deal with this possibility ...
-        //if (DEBUG) debugCurrentTargets(sources);
 
-        // currentTotalTargetsAssigned: new Array(sources.length),
-        // currentTotalScore:  new Array(sources.length)
-
-        counter++;
-        // need to increment the position and go back if we reach the end
+        var badCandidate=false;
         do {
+            numberOperation++;
+            var goingBack=false;
+            // before changing anything we need to unset targets assignment;
+            if (! badCandidate) unsetTargetAssignment(sources, targets, currentSource);
+            badCandidate=false;
             sources[currentSource].currentTargetPosition++;
-        } while (
-            sources[currentSource].currentTargetPosition===sources[currentSource].possibleTargets.length &&
-                --currentSource>=0
-            )
+            // need to recalculate the information for this new hypothesis
+            if (sources[currentSource].currentTargetPosition===sources[currentSource].possibleTargets.length) {
+                // we tried all the possibilities, we need to go back
+                currentSource--;
+                goingBack=true;
+            } else {
+                var source=sources[currentSource];
+                if (targetsNotAssigned(targets, source.possibleTargets[source.currentTargetPosition])) {
+                    
+                    
+                    source.currentTotalTargetsAssigned=(currentSource>0) ? sources[currentSource-1].currentTotalTargetsAssigned:0;
+                    source.currentTotalTargetsAssigned+=source.possibleTargets[source.currentTargetPosition].length;
+
+                 //   console.log(currentSource, source.currentTotalTargetsAssigned, (sources.length-currentSource) * maxTarget, numberOfTargets)
+                    
+                    if ((source.currentTotalTargetsAssigned + (sources.length-currentSource-1) * maxTarget) < numberOfTargets) { // can we still assign all the targets ?
+                 //       console.log("Don't check");
+                        badCandidate=true;
+                    } else {
+                        // CALCULATE SCORE
+                        source.currentTotalScore=(currentSource>0) ? sources[currentSource-1].currentTotalScore:1;
+                        source.currentTotalScore*=0.9;
+                        
+                        // we could still specify it is a bad candidate based on the score for example
+                        badCandidate=false;
+                    }
+                } else {
+                    badCandidate=true;
+                }
+                if (! badCandidate) {
+                    setTargetAssignment(sources, targets, currentSource);
+                }
+            }
+            // console.log('badCandidate: '+badCandidate, 'goingBack: '+goingBack, currentSource);
+        } while ((badCandidate || goingBack) && currentSource>=0);
+
+
+        if (!badCandidate && currentSource === (sources.length-1)) {
+            // we have a candidate !
+            counter++;
+            if (DEBUG) debugCurrentTargets(sources, targets);
+        }
+    } while (limit-- > 0 && currentSource >= 0);
+
+    function targetsNotAssigned(targets, array) {
+        for (var id of array) {
+            if (targets[id].isUsed) return false;
+        }
+        return true;
     }
     
-    console.log("Total: ",counter)
+    function unsetTargetAssignment(sources, targets, currentSource) {
+        var source=sources[currentSource];
+        if (! source || ! source.possibleTargets[source.currentTargetPosition]) return;
+        source.possibleTargets[source.currentTargetPosition].forEach(function(id) {
+            if (DEBUG) console.log('unset: ',id);
+            targets[id].isUsed=false;
+        });
+    }
+
+    function setTargetAssignment(sources, targets, currentSource) {
+        var source=sources[currentSource];
+        if (! source || ! source.possibleTargets[source.currentTargetPosition]) return;
+        source.possibleTargets[source.currentTargetPosition].forEach(function(id) {
+            if (DEBUG) console.log('set: ',id);
+            targets[id].isUsed=true;
+        });
+    }
     
-    function debugCurrentTargets(sources) {
+    function debugCurrentTargets(sources, targets) {
         console.log('----------- Source current association ----------')
         for (var i=0; i<sources.length; i++) {
             var source=sources[i];
-            console.log(source.currentTargetPosition, source.possibleTargets[source.currentTargetPosition]);
+            console.log(
+                source.currentTargetPosition,
+                source.possibleTargets[source.currentTargetPosition],
+                'Total targets assigned:'+source.currentTotalTargetsAssigned);
         }
+        var usedTargets=[];
+        for (var key in targets) {
+            if (targets[key].isUsed) usedTargets.push(key);
+        }
+        console.log("Used targets: ", usedTargets);
     }
 
-    
-    return;
+    console.log("Number of operation", numberOperation);
+    console.log("Total: ",counter)
+    return counter;
     
 
     var sourcePos = {};
@@ -247,6 +318,6 @@ function associator(links, options) {
 
 
 function getScore() {
-    return 100;
+    return 1;
 }
 
--- test/index.js
+++ test/index.js
@@ -2,14 +2,174 @@
 
 var associator = require('..');
 
-describe.only('Test the associator', function () {
-
-    var result=associator({a:[1,2,3], b:[1, 2, 3], c:[5, 6,2]}, {
-        minTarget: 2,
-        maxTarget: 2
+describe('Test the associator', function () {
+    
+    it('no overlapping target', function () {
+        var result=associator({a:[1,2,3], b:[4,5,6], c:[7,8]}, {
+            minTarget: 2,
+            maxTarget: 2
+        }); 
+        result.should.equal(0);
     });
 
-    it('should be tested', function () {
-        
+    it('with overlapping targets', function () {
+        var result=associator({a:[1,2,3], b:[2,5,6], c:[7,8]}, {
+            minTarget: 2,
+            maxTarget: 2
+        });
+        result.should.equal(0);
     });
+
+    it('with no overlapping and one result', function () {
+        var result=associator({a:[1,2], b:[3,4], c:[5,6]}, {
+            minTarget: 2,
+            maxTarget: 2
+        });
+        result.should.equal(1);
+    });
+
+    it('with no overlapping and one result but couple of branches', function () {
+        var result=associator({a:[1,2], b:[3,4]}, {
+            minTarget: 1,
+            maxTarget: 2
+        });
+        result.should.equal(1);
+    });
+
+    it('with no overlapping and one result but many branches', function () {
+        var result=associator({a:[1,2], b:[3,4], c:[5,6], d:[7,8]}, {
+            minTarget: 0,
+            maxTarget: 2
+        });
+        result.should.equal(1);
+    });
+
+    it('with one overlapping and impossible result', function () {
+        var result=associator({a:[1,2], b:[2,3]}, {
+            minTarget: 2,
+            maxTarget: 2
+        });
+        result.should.equal(0);
+    });
+
+    it('with 2 overlapping and possible results', function () {
+        var result=associator({a:[1,2], b:[2,3,4]}, {
+            minTarget: 2,
+            maxTarget: 2
+        });
+        result.should.equal(1);
+    });
+
+
+    it('Large test 5544 possibilities but impossible result', function () {
+        var result=associator({a:[1,2,3,4,5,6,7,8], b:[9,10,11], c:[12,13], d:[14,15,16,17,18,19,20,21,22,23,24,25]}, {
+            minTarget: 2,
+            maxTarget: 2
+        });
+        result.should.equal(0);
+    });
+
+    it('Small test with many assignment', function () {
+        var result=associator(
+            {
+                a:[1,2,3,4],
+                b:[1,2,3,4]
+            }, {
+            minTarget: 2,
+            maxTarget: 2
+        });
+        result.should.equal(6);
+    });
+
+    it('Small test with many assignment and minTarget=0', function () {
+        var result=associator(
+            {
+                a:[1,2,3,4],
+                b:[1,2,3,4]
+            }, {
+                minTarget: 0,
+                maxTarget: 2
+            });
+        result.should.equal(6);
+    });
+
+    it('Small test with many assignment and 3 min / max', function () {
+        var result=associator(
+            {
+                a:[1,2,3,4,5,6],
+                b:[1,2,3,4,5,6]
+            }, {
+                minTarget: 3,
+                maxTarget: 3
+            });
+        result.should.equal(20);
+    });
+
+    it('Large test with many assignment and 2 min / max - 45*28*15*6 possibilities', function () {
+        var result=associator(
+            {
+                a:[1,2,3,4,5,6,7,8,9,10],
+                b:[1,2,3,4,5,6,7,8,9,10],
+                c:[1,2,3,4,5,6,7,8,9,10],
+                d:[1,2,3,4,5,6,7,8,9,10],
+                e:[1,2,3,4,5,6,7,8,9,10]
+
+            }, {
+                minTarget: 2,
+                maxTarget: 2
+            });
+        result.should.equal(113400);
+    });
+
+    it('Large test with many assignment and 0 min / 2 max - 45*28*15*6 possibilities', function () {
+        var result=associator(
+            {
+                a:[1,2,3,4,5,6,7,8,9,10],
+                b:[1,2,3,4,5,6,7,8,9,10],
+                c:[1,2,3,4,5,6,7,8,9,10],
+                d:[1,2,3,4,5,6,7,8,9,10],
+                e:[1,2,3,4,5,6,7,8,9,10]
+
+            }, {
+                minTarget: 0,
+                maxTarget: 2
+            });
+        result.should.equal(113400);
+    });
+    
+
+    it('Small test with 90 assignments and 2 min / max', function () {
+        var result=associator(
+            {
+                a:[1,2,3,4,5,6],
+                b:[1,2,3,4,5,6],
+                c:[1,2,3,4,5,6]
+            }, {
+                minTarget: 0,
+                maxTarget: 2
+            });
+        result.should.equal(90);
+    });
+
+    it('Large (10) sequential set', function () {
+        var result=associator(
+            {
+                a:[1,2],
+                b:[1,2,3,4],
+                c:[3,4,5,6],
+                d:[5,6,7,8],
+                e:[7,8,9,10],
+                f:[9,10,11,12],
+                g:[11,12,13,14],
+                h:[13,14,15,16],
+                i:[15,16,17,18],
+                j:[17,18,19,20]
+            }, {
+                minTarget: 0,
+                maxTarget: 2
+            });
+        result.should.equal(1);
+    });
+    
+    
 });
